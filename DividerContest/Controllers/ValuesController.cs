7cb654db3b4a362eb21227a7de076d97860c5c7e
Kirill Fimchenko <frimko@gmail.com>
2018-07-20 23:28:31 +0400

    init

---
+++ DividerContest/Controllers/ValuesController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DividerContest.Controllers
+{
+   // [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [Route("")]
+        [HttpGet]
+        public string Get(decimal a, decimal b)
+        {
+            return (a / b).ToString();
+        }
+    }
+}
